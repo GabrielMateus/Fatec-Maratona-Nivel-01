80d39b508df838dc28944ac50bdb11f174d0c925
Gabriel Mateus <gmateus2012@gmail.com>
2022-10-14 23:57:24 -0300

    Exercicio16 - 1078

---
+++ Ex16_1078.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+int main() {
+  int n1,n2;
+  
+  scanf("%d", &n1);
+  n2 = 1*n1;
+  
+  printf("1 x %d = %d\n", n1, n2);
+  n2= 2*n1;
+  printf("2 x %d = %d\n", n1, n2);
+  n2= 3*n1;
+  printf("3 x %d = %d\n", n1, n2);
+  n2= 4*n1;
+  printf("4 x %d = %d\n", n1, n2);
+  n2= 5*n1;
+  printf("5 x %d = %d\n", n1, n2);
+  n2= 6*n1;
+  printf("6 x %d = %d\n", n1, n2);
+  n2= 7*n1;
+  printf("7 x %d = %d\n", n1, n2);
+  n2= 8*n1;
+  printf("8 x %d = %d\n", n1, n2);
+  n2= 9*n1;
+  printf("9 x %d = %d\n", n1, n2);
+  n2= 10*n1;
+  printf("10 x %d = %d\n",n1, n2);
+ 
+    return 0;
+}
